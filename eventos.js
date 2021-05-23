338fde84bbab1b07e0cbf2ff00c0e9bccd94922b
Allan Douglas Santos Tavares <allan.tavares@estudante.ufla.br>
2021-05-23 09:49:44 -0300

    modificando altura e largura das div's

--- eventos.js
+++ eventos.js
@@ -1,6 +1,8 @@
- function modificaEstilo(corDefundo){
+ function modificaEstilo(corDefundo ,l , a){
     
    document.getElementById('quadrado').style.background = corDefundo  
+   document.getElementById('quadrado').style.height = l 
+   document.getElementById('quadrado').style.width = a 
 
    
 
